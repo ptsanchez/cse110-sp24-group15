6a05199ad584cfe97013a330a129fcb3df6a79e5
David <kohdavid31@gmail.com>
2024-05-29 18:11:35 -0700

    added some of the tests

--- project/tests/week_page.tests.js
+++ project/tests/week_page.tests.js
@@ -1,2 +1,46 @@
+// Import the functions
+const { switchWeekly, switchMonthly, redirectToAddLogPage, calendarScript, } = require('../weekPage/week_page');
 
+// Mock the global window object
+global.window = {
+    location: {
+      href: ''
+    }
+  };
 
+describe('Click week button', () => {
+    test('Window redirects to week page when switchWeekly is called', () => {
+      switchWeekly();
+  
+      // Ensure window.location.href is set to the day page URL
+      expect(window.location.href).toBe('../weekPage/week_page.html');
+    });
+});
+
+describe('Click month button', () => {
+    test('Window redirects to month page when switchMonthly is called', () => {
+        switchMonthly();
+  
+      // Ensure window.location.href is set to the day page URL
+      expect(window.location.href).toBe('../monthPage/month_page.html');
+    });
+});
+
+describe('Click add log button', () => {
+    test('Window redirects to add log page when redirectToAddLogPage is called', () => {
+      redirectToAddLogPage();
+  
+      // Ensure window.location.href is set to the day page URL
+      expect(window.location.href).toBe('../addLogPage/add_log_page.html');
+    });
+});
+
+describe('Click a day', () => {
+    test('Window redirects to day page when a day is clicked', () => {
+      const day = document.querySelector('day-column');
+      day.click();
+  
+      // Ensure window.location.href is set to the day page URL
+      expect(window.location.href).toBe('../dayPage/day_page.html');
+    });
+});
