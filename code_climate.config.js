6dd56a03367ff761a4d7955cf94e77495599a5e1
AdvaithRavishankar <83779786+AdvaithRavishankar@users.noreply.github.com>
2024-05-30 13:31:49 -0700

    Update code_climate.config.js

--- code_climate.config.js
+++ code_climate.config.js
@@ -1,15 +1,13 @@
 // code_climate.config.js
-module.exports = [
-    {
-      files: ["project/**/*.js", "project/**/*.html", "project/**/*.css"],
-      languageOptions: {
-        ecmaVersion: 2021,
-        sourceType: "module",
-      },
-      rules: {
-        // Add your ESLint rules here
-        "no-unused-vars": "warn",
-        "no-console": "off",
-      },
-    },
-  ];
+module.exports = {
+  files: ["project/**/*.js", "project/**/*.html"],
+  languageOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  rules: {
+    "no-unused-vars": "warn",
+    "no-console": "off",
+  },
+};
+
