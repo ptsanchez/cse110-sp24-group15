e8498ab2cffb66b85423564057c24b0ddad54d61
cmy0357 <mic015@ucsd.edu>
2024-05-29 19:55:15 -0700

    Refactored code and added comments

--- project/addProjectPage/add_project_page.js
+++ project/addProjectPage/add_project_page.js
@@ -1,47 +1,71 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', () => {
+/**
+ * Create a template JSON string with new project data
+ * @param {object} formObj - Data of new project
+ * @returns {string} A JSON string ready to be stored in local storage
+ */
+function createProjectData(formObj) {
+    let projectsObj = {
+        current_project: '', // TODO 
+        current_date: (new Date()).toLocaleDateString(),
+        project_data: {
+            project_1: {
+                projectName: formObj['project-name'], 
+                projectTag: formObj['project-tag'], 
+                projectContributors: formObj['project-contributor'], 
+                projectDescription: formObj['project-description'], 
+                active: true,
+                logs: {}, // TODO
+                BranchLink: '', // TODO
+                TodoList: {} // TODO
+            }
+        }
+    };
+    return JSON.stringify(projectsObj);
+}
+
+/**
+ * Update JSON string to include new project data
+ * @param {object} formObj - Data of new project
+ * @param {string} localStr - The JSON string obtained from local storage
+ * @returns {string} A JSON string ready to be stored in local storage
+ */
+function updateProjectData(formObj, localStr) {
+    let projectsObj = JSON.parse(localStr);
+    const newIndex = Object.keys(projectsObj.project_data).length;
+    const newName = `project_${newIndex + 1}`;
+    projectsObj.project_data[newName] = {
+        projectName: formObj['project-name'], 
+        projectTag: formObj['project-tag'], 
+        projectContributors: formObj['project-contributor'], 
+        projectDescription: formObj['project-description'], 
+        active: true,
+        logs: {}, // TODO
+        BranchLink: '', // TODO
+        TodoList: {} // TODO
+    }
+    return JSON.stringify(projectsObj);
+}
+
+/**
+ * Update local storage with submitted form data
+ * @param {HTMLElement} form - Form element
+ */
+function handleSubmission(form) {
     const formData = new FormData(form);
     const formObj = {};
     for (const pair of formData.entries()) {
         formObj[pair[0]] = pair[1];
     }
     let localStr = localStorage.getItem('projects'); // TODO: Change key accordingly
-    var projectsObj = {};
-    // Create JSON string if it does not exist
     if (localStr == null) {
-        projectsObj = {
-            current_project: '', // TODO 
-            current_date: (new Date()).toLocaleDateString(),
-            project_data: {
-                project_1: {
-                    projectName: formObj['project-name'], 
-                    projectTag: formObj['project-tag'], 
-                    projectContributors: formObj['project-contributor'], 
-                    projectDescription: formObj['project-description'], 
-                    active: true,
-                    logs: {}, // TODO
-                    BranchLink: '', // TODO
-                    TodoList: {} // TODO
-                }
-            }
-        };
+        localStr = createProjectData(formObj);
+    } else {
+        localStr = updateProjectData(formObj, localStr);
     }
-    // Update project_data otherwise
-    else {
-        projectsObj = JSON.parse(localStr);
-        const newIndex = Object.keys(projectsObj.project_data).length;
-        const newName = `project_${newIndex + 1}`;
-        projectsObj.project_data[newName] = {
-            projectName: formObj['project-name'], 
-            projectTag: formObj['project-tag'], 
-            projectContributors: formObj['project-contributor'], 
-            projectDescription: formObj['project-description'], 
-            active: true,
-            logs: {}, // TODO
-            BranchLink: '', // TODO
-            TodoList: {} // TODO
-        }
-    }
-    localStr = JSON.stringify(projectsObj);
     localStorage.setItem('projects', localStr);
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', () => {
+    handleSubmission(form);
 });
---
+++ project/tests/add_project_page.tests.js
@@ -0,0 +1,6 @@
+// Import the functions
+const { createProjectData, updateProjectData } = require('../addProjectPage/add_project_page');
+
+describe('Form validation', () => {
+    
+});
