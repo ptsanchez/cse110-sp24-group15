a227370684360dbf5043a25876bf2efebc21d3a6
rabongHan <recognize0117@gmail.com>
2024-06-03 16:53:18 -0700

    remove code_snippet.js and directly import script to html

--- project/addLogPage/code_snippet.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    window.editor = CodeMirror.fromTextArea(document.getElementById("log-code-snippet"), {
-      lineNumbers: true,
-      mode: "javascript",
-      theme: "dracula",
-      tabSize: 2,
-      lineWrapping: true
-    });
-}
