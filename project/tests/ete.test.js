3d34b0c925e659ffb015b76b7ff92e62238d4786
rabongHan <recognize0117@gmail.com>
2024-06-09 20:46:26 -0700

    changed getTodayDate()

--- project/tests/ete.test.js
+++ project/tests/ete.test.js
@@ -43,11 +43,8 @@ describe('Developer Journal Flow', () => {
     }
 
     function getTodayDate() {
-        const today = new Date();
-        const dd = String(today.getDate()).padStart(2, '0');
-        const mm = String(today.getMonth() + 1).padStart(2, '0'); 
-        const yyyy = today.getFullYear();
-        return `${yyyy}-${mm}-${dd}`;
+        const today = new Date().toLocaleDateString(`en-CA`);
+        return today;
     }
 
     // Test 1 
