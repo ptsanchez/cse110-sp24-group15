356b217e0d3360394c97e86f4dc885ed6c390614
rabongHan <recognize0117@gmail.com>
2024-06-09 22:55:18 -0700

    added getDate() to currentday (missed)

--- project/tests/ete.test.js
+++ project/tests/ete.test.js
@@ -220,7 +220,7 @@ describe('Developer Journal Flow', () => {
 
         // Click current day in calendar in month page
         await page.waitForSelector('.month-calendar-title');
-        const currentDay = new Date();
+        const currentDay = new Date().getDate();
         await page.waitForSelector('.dates li')
         await page.evaluate((currentDay) => {
             const dates = document.querySelectorAll('.dates li');
@@ -383,7 +383,7 @@ describe('Developer Journal Flow', () => {
         
         // Click current day in calendar in month page
         await page.waitForSelector('.month-calendar-title');
-        const currentDay = new Date();
+        const currentDay = new Date().getDate();
         await page.waitForSelector('.dates li')
         await page.evaluate((currentDay) => {
             const dates = document.querySelectorAll('.dates li');
